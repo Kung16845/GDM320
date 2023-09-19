a80d400e45a8979c5e39f00514d38816bf981679
Kung16845 <111451858+Kung16845@users.noreply.github.com>
2023-09-19 09:11:11 +0700

    ADD Sprite

--- My project/Assets/Script/Movement/PlayerMovement.cs
+++ My project/Assets/Script/Movement/PlayerMovement.cs
@@ -19,7 +19,7 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate() 
     {   
         SetPlayerVelocity();
-        RotateInDirecttionOfInput();
+        RotateInDirecttionOfInput();     
     }
     private void SetPlayerVelocity() 
     {
