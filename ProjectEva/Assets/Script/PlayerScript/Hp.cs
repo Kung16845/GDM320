8e46a5e4856ae1c692b81cc08d424f580293538e
Agentatnaf <111513753+AgentAtnaf@users.noreply.github.com>
2023-10-16 20:30:18 +0700

    Add new scene for play and add gameover condition.

--- ProjectEva/Assets/Script/PlayerScript/Hp.cs
+++ ProjectEva/Assets/Script/PlayerScript/Hp.cs
@@ -1,8 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Hp : MonoBehaviour
 {
@@ -12,8 +11,11 @@ public class Hp : MonoBehaviour
     public TextMeshProUGUI healthText;
     public CanvasGroup canvasGroup; // Reference to the CanvasGroup component.
     public float lowHpThreshold = 30f; // HP threshold for triggering the alpha change.
+    public ReloadSceneOnZeroHP reloadScript;
 
     private SanityScaleController sanityScaleController;
+    private float timeRemaining = 3f; // Time remaining before reloading the scene.
+    private bool isReloading = false;
 
     void Start()
     {
@@ -37,6 +39,12 @@ public class Hp : MonoBehaviour
             // Reset the alpha to full when HP is above the threshold.
             canvasGroup.alpha = 0f;
         }
+
+        if (currenthp <= 0f && !isReloading)
+        {
+            isReloading = true;
+            reloadScript.StartReloadScene(); // Call the function in the ReloadSceneOnZeroHP script.
+        }
     }
 
     public void TakeDamage(float damage)
@@ -48,18 +56,35 @@ public class Hp : MonoBehaviour
         }
         else
         {
-            player.SetActive(false);
+            // Call the StartReloadScene function when HP reaches 0.
+            reloadScript.StartReloadScene();
         }
     }
 
     public void HealHp(float healAmount)
     {
         currenthp += healAmount;
-        // currenthp = Mathf.Clamp(currenthp, 0, maxhp);
+        currenthp = Mathf.Clamp(currenthp, 0, maxhp);
     }
     
     void UpdateHealthText()
     {
         healthText.text = "HP: " + currenthp.ToString("F2");
+    }
+
+    void StartReloadSceneTimer()
+    {
+        InvokeRepeating("UpdateTimer", 1f, 1f);
+    }
+
+    void UpdateTimer()
+    {
+        timeRemaining -= 1f;
+
+        if (timeRemaining <= 0f)
+        {
+            // Reload the scene
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 }
