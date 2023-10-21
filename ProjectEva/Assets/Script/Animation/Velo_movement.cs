acbfd746cf6014e7771e9ac72b94bd6dbb1d6ab9
LZsolar <pornchanok.char@mail.kmutt.ac.th>
2023-10-21 22:19:22 +0700

    ADD: แงงงุมยังเดินไม่ตรง

---
+++ ProjectEva/Assets/Script/Animation/Velo_movement.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Velo_movement : MonoBehaviour
+{
+    Animator animator;
+    Transform mon;
+    Vector2 movement;
+    Vector2 lastPosition;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        mon = GameObject.FindGameObjectWithTag("Velocrux").transform;
+        lastPosition =new Vector2(0,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector2 currentPosition = mon.position;
+        
+        if (currentPosition != lastPosition) {
+            
+            animator.SetFloat("Speed", 1); 
+        }
+        else {
+
+            animator.SetFloat("Speed", 0); 
+        }
+
+        movement.x = Mathf.Cos(Mathf.Deg2Rad * Vector2.Angle(lastPosition, currentPosition));
+        movement.y = Mathf.Cos(Mathf.Deg2Rad * Vector2.Angle(lastPosition, currentPosition));
+
+
+
+
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+
+        lastPosition = mon.position;
+
+        transform.position = new Vector3(mon.position.x, mon.position.y);
+    }
+}
