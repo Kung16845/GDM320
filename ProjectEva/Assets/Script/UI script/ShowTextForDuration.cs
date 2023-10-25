044cd7eef69d4742f3109d99638cbb03a10ee25c
AgentAtnaf <111513753+AgentAtnaf@users.noreply.github.com>
2023-10-25 11:11:09 +0700

    Main0.3 complete missing animation attack

--- ProjectEva/Assets/Script/UI script/ShowTextForDuration.cs
+++ ProjectEva/Assets/Script/UI script/ShowTextForDuration.cs
@@ -1,55 +1,52 @@
 using UnityEngine;
 using TMPro;
 
-public class ShowTextForDuration : MonoBehaviour
+public class ShowPanelForDuration : MonoBehaviour
 {
-    public TextMeshProUGUI textMeshPro; // Reference to the TMP UI element in the Inspector.
-    public float displayDuration = 1.0f; // Customizable display duration in seconds.
-
-    private float displayTimer; // Timer to keep track of display time.
-    private bool isDisplaying; // Flag to indicate if the text is currently displayed.
+    public GameObject panel; // Reference to the panel GameObject in the Inspector.
+    
+    private bool isDisplaying; // Flag to indicate if the panel is currently displayed.
 
     void Start()
     {
-        // Initialize the timer and set the text to inactive.
-        displayTimer = 0f;
         isDisplaying = false;
-
-        // You can optionally hide the text at the start.
-        textMeshPro.gameObject.SetActive(false);
-
-        // Call a method to start displaying the text.
-        StartDisplayingText();
+        panel.SetActive(false);
+        StartDisplayingPanel();
     }
 
     void Update()
     {
         if (isDisplaying)
         {
-            // Update the timer while the text is displayed.
-            displayTimer += Time.deltaTime;
-
-            // Check if it's time to hide the text.
-            if (displayTimer >= displayDuration)
+            // Check for a mouse click to continue.
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-                StopDisplayingText();
+                StopDisplayingPanel();
             }
+        }
+        else
+        {
+            // Resume normal scene operations here.
         }
     }
 
-    void StartDisplayingText()
+    void StartDisplayingPanel()
     {
-        // Set the text to active and reset the timer.
-        textMeshPro.gameObject.SetActive(true);
-        displayTimer = 0f;
+        // Pause scene by setting Time.timeScale to 0.
+        Time.timeScale = 0;
+        
+        // Set the panel to active.
+        panel.SetActive(true);
         isDisplaying = true;
     }
 
-    void StopDisplayingText()
+    void StopDisplayingPanel()
     {
-        // Set the text to inactive and reset the timer.
-        textMeshPro.gameObject.SetActive(false);
-        displayTimer = 0f;
+        // Resume scene by setting Time.timeScale to 1.
+        Time.timeScale = 1;
+        
+        // Set the panel to inactive.
+        panel.SetActive(false);
         isDisplaying = false;
     }
 }
