dfaf107d74252bb78a524740e8b4996c6fb2e597
Kung16845 <111451858+Kung16845@users.noreply.github.com>
2023-11-25 15:42:29 +0700

    Begin SaveAndLoad Level 1

---
+++ ProjectEva/Assets/Script/SaveAndLoadGame/DatabeforeJson.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+[Serializable] 
+public class DataSave
+{   
+    [Header("DataPlayer")]
+    public float currentHpPlayer;
+    public float transformPlayerX;
+    public float transformPlayerY;
+    
+    [Header("DataEnemy")]
+    public float currentHpEnemy;
+    public float transformEnemyX;
+    public float transformEnemyY;
+}
