7af95aae5eb093e0e6bba2fdd14ee04bbd19ead3
Kung16845 <111451858+Kung16845@users.noreply.github.com>
2023-11-26 19:50:18 +0700

    test fix scriptableobject

--- ProjectEva/Assets/Script/Inventory/ItemCharactor/InventoryPresentCharactor.cs
+++ ProjectEva/Assets/Script/Inventory/ItemCharactor/InventoryPresentCharactor.cs
@@ -261,25 +261,4 @@ public class InventoryPresentCharactor : MonoBehaviour
     }
 }
 
-[CreateAssetMenuAttribute(menuName = "ScriptableObject object/ItemChractor")]
-[Serializable]
-public class ItemsDataCharactor : ScriptableObject
-{
-    public string nameItemCharactor;
-    public Sprite ItemImage;
-    public string scriptItem;
-    public int count;
-    public int maxCount;
-    public bool isFlashLight;
-    public bool isOnhand;
-    // public void Initialize(string nameItem, Sprite sprite, int countItem, int maxCountItem, string scriptItemCharactor, bool flashLight, bool Onhand)
-    // {
-    //     nameItemCharactor = nameItem;
-    //     ItemImage = sprite;
-    //     count = countItem;
-    //     maxCount = maxCountItem;
-    //     scriptItem = scriptItemCharactor;
-    //     isFlashLight = flashLight;
-    //     isOnhand = Onhand;
-    // }
-}
+
