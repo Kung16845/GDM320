874a5b9825bcc501fa150a4ac72de3203c067e26
Kung16845 <111451858+Kung16845@users.noreply.github.com>
2023-12-12 14:00:37 +0700

    fix bug load scean and New game Next Scean

--- ProjectEva/Assets/Script/LoadScean.cs
+++ ProjectEva/Assets/Script/LoadScean.cs
@@ -2,32 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class LoadScene: MonoBehaviour
+public class LoadScene : MonoBehaviour
 {
     public SaveManager saveManager;
+    public bool isNewScean;
+    public bool isLoadScean;
+    private void Start()
+    {
+        // Make sure the saveManager persists between scenes
+        DontDestroyOnLoad(this.gameObject);
+    }
     public void LoadDataNextScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-       LoadDataAfterSceneLoaded();
+        isLoadScean = true;
+        
     }
     public void LoadNextScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        isNewScean = true;
     }
     public void ExitGame()
     {
         Application.Quit();
     }
-    public void LoadDataAfterSceneLoaded()
-    {
-        StartCoroutine(LoadDataCoroutine());
-    }
 
-    private IEnumerator LoadDataCoroutine()
-    {
-        yield return new WaitForSeconds(2f); // Wait for a short time to ensure the scene is loaded
-
-        // Now, you can safely call saveManager.AllLoad() after the new scene has been loaded
-        saveManager.AllLoad();
-    }
 }
